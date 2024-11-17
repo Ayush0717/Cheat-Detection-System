4a470568ff41c1792985c21a0243c01608643d62
Unisha Rozzi <unishatirkey08@gmail.com>
2024-11-17 19:12:20 +0530

    Added monitor

--- run_game.py
+++ run_game.py
@@ -2,8 +2,10 @@ import os
 import sys
 
 # Add the project root directory to Python path
-project_root = os.path.dirname(os.path.abspath(/Users/ayushgoel/OS Project/Cheat-Detection-System/run_game.py))
+project_root = os.path.dirname(os.path.abspath("/Users/ayushgoel/OS Project/Cheat-Detection-System/run_game.py"))
 sys.path.append(project_root)
 
-# Import and run the game
+# Import and run the game and monitor
 from game.game_ayush import *
+from cheat_detection.run_monitor import *  
+
